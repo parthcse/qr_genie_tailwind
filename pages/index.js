aabb445f2d905857936b8b85dd04fde794236ef9
parthcse <parth.cse15@gmail.com>
2026-01-19 16:26:23 +0530

    Testing file updation using CI/CD

--- pages/index.js
+++ pages/index.js
@@ -233,7 +233,7 @@ export default function Landing({ initialUser }) {
       <div className="bg-white">
         <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8 text-center">
           <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-            <span className="block">QR Codes That Work</span>
+            <span className="block">QR Codes That Work Attempt 7</span>
             <span className="block text-indigo-600">For Your Business</span>
           </h1>
           <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
