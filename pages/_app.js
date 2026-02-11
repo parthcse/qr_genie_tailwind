a54040a4ab314e0ad66fd629cfc0e73b40f577be
parthcse <parth.cse15@gmail.com>
2026-02-11 15:01:56 +0530

    Favicon Added

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,16 @@
-
+import Head from "next/head";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
+        <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
